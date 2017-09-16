8245b3444a7c8acaef262e5b102922c3ac9b142d
SkalskiP <pskalski@student.agh.edu.pl>
2017-09-17 01:22:43 +0200

    my own linear regression remade

--- 01_Simple_Linear_Regression/Simple_Linear_Regression.py
+++ 01_Simple_Linear_Regression/Simple_Linear_Regression.py
@@ -1,49 +1,54 @@
 # SIMPLE LINEAR REGRESSION
+# Metoda przyjmuje jako argumenty dwa numpy array'e
+
+import numpy as np
+
 
 class MySimpleLinearRegression:
     
-    def __init__(self, fit_intercept=True, normalize=False, copy_X=True, n_jobs=1):
+    def __init__(self, fit_intercept=True):
+        # zmienna informująca o tym czy należy obliczać wpółczynnik punktu /
+        # przecięcia z osią y
         self.fit_intercept = fit_intercept
-        self.normalize = normalize
-        self.copy_X = copy_X
-        self.n_jobs = n_jobs
     
     def fit(self, X, y, sample_weight=None):
-        if(len(X) != len(y)):
-            raise ValueError("X and y should have equal length")
-        else:            
-            # mean values of lists X and y
-            X_mean = sum(X)/len(X)
-            y_mean = sum(y)/len(y)
+        # weryfikujemy czy dostarczone dane mają odpowiedni format
+        # X musi być macierzą o wymiarach (R, 1)
+        # Y musi być wektorem o wymiarach (R,)
+        
+        if X.ndim == 2 and y.ndim == 1 and X.size == y.size:
+            # przy pomocy metody najmniejszych kwadratów obliczamy /
+            # współczynnik nachylenia prostej coef_
             
+            # srednia wartoć wektorów X oraz Y
+            X_mean = np.sum(X)/np.size(X)
+            y_mean = np.sum(y)/np.size(y)
+            
+            # licznik
             nominator = 0
+            # mianownik
             denominator = 0
             
-            for X_i, y_i in zip(X, y):
-                nominator += (X_i - X_mean)*(y_i - y_mean)
-                denominator += (X_i - X_mean)**2
+            for i in range(X.size):
+                     
+                nominator += (X[i][0] - X_mean) * (y[i] - y_mean)
+                denominator += (X[i][0] - X_mean)**2
             
+            # współczynnik nachylenia prostej coef_
             self.coef_ = nominator/denominator
             
-            if(self.fit_intercept):
-                self.intercept_ = y_mean - self.coef_ * X_mean
+            if self.fit_intercept:
+                # współczynnikpunktu przecięcia z osią y
+                self.intercept_ = y_mean - np.dot(X_mean, self.coef_)
                 
-    def predict(self, X):
-        return [self.intercept_ + self.coef_ * x for x in X]
+        else:
+            raise ValueError("Matrix size is incorrect")
         
-"""               
-X = [1.1, 1.3, 1.5, 2.0, 2.2, 2.9, 3.0, 3.2, 3.2, 3.7, 
-     3.9, 4.0, 4.0, 4.1, 4.5, 4.9, 5.1, 5.3, 5.9, 6.0, 
-     6.8, 7.1, 7.9, 8.2, 8.7, 9.0, 9.5, 9.6, 10.3, 10.5]
-Y = [39343.0, 46205.0, 37731.0, 43525.0, 39891.0, 56642.0,
-     60150.0, 54445.0, 64445.0, 57189.0, 63218.0, 55794.0,
-     56957.0, 57081.0, 61111.0, 67938.0, 66029.0, 83088.0,
-     81363.0, 93940.0, 91738.0, 98273.0, 101302.0, 113812.0,
-     109431.0, 105582.0, 116969.0, 112635.0, 122391.0, 121872.0]
-
-test = MySimpleLinearRegression()
-test.fit(X, Y)
-print(test.coef_)
-print(test.intercept_)
-print(test.predict([5, 2]))
-"""
+import pandas as pd
+dataset = pd.read_csv('Salary_Dataset.csv')
+X = dataset.iloc[:, :-1].values
+Y = dataset.iloc[:, 1].values
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 1/3, random_state = 0)
+my_regressor = MySimpleLinearRegression()
+my_regressor.fit(X_train, y_train)
