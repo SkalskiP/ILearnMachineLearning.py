0b7f7f63f61e9ca34ed3885c858d98807cf420ca
SkalskiP <pskalski@student.agh.edu.pl>
2017-09-21 00:00:43 +0200

    K-Means code update

--- 02_K-Means/K-Means.py
+++ 02_K-Means/K-Means.py
@@ -3,6 +3,11 @@ import pandas as pd
 import random
 
 def kmeans(k, epsilon = 0, distance = 'euclidian'):
+    """
+    k - iloć klastrów na jaką podzielimy nasze dane
+    epsilon - minimalny dopuszczalny przez nas błąd, po przekroczeniu tego progu program zostanie wstrzymany
+    distance - sposób obliczania odległoci pomiędzy punktami
+    """
     # list to store past centroid
     history_centroids = []
     # set the distance calculation type
@@ -19,6 +24,41 @@ def kmeans(k, epsilon = 0, distance = 'euclidian'):
         # to keep track of centroid at every iteration
         prototypes_old = np.zeros(prototypes.shape)
         # to store clusters
-        belongs_to = np.zeros((num-instances, 1))
+        belongs_to = np.zeros((num_instances, 1))
         norm = dist_method(prototypes, prototypes_old)
         iteration = 0
+        
+        while norm > epsilon:
+            iteration += 1
+            norm = dist_method(prototypes, prototypes_old)
+            # for each instnce in the dataset
+            for index_instance, instance in enumerate(dataset):
+                # find a distance vector of size k
+                dist_vec = np.zeros((k, 1))
+                # for each centroid
+                for index_prototype, prototype in enumerate(prototypes):
+                    # compute the distance between datapoint and centroid
+                    dist_vec[index_prototype] = dist_method(prototype, instance)
+                # find the smallest distance, assign the distance to a cluster
+                belongs_to[index_instance, 0]  = np.aargmin(dist_vec)
+                
+            tmp_prototypes = np.zero((k, num_features))
+            
+            # for each cluster, k of them
+            for index in range(len(prototypes)):
+                # get all the points assigned to a cluster
+                instances_close = [i for i in range(len(belongs_to)) if belongs_to[i] == index]
+                #find the mean of those points, this is our new centroid
+                prototype = np.mean(dataset[instances_close], axis = 0)
+                # add our new centroid to our new temporary list
+                tmp_prototypes[index, :] = prototype
+                
+            # set the new list to the current list
+            prototypes = tmp_prototypes
+            
+            #add our calculated centroids to our history for plotting
+            history_centroids.append(tmp_prototypes)
+            
+        # return calculated centroids, history of them all, and assignment for
+        return prototypes, history_centroids, belongs_to
+    
