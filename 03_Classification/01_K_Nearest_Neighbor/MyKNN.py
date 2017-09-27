fa2fafa84440c91e240e8e97d7239e05dd4c5d06
SkalskiP <pskalski@student.agh.edu.pl>
2017-09-27 20:24:55 +0200

    Iris KNN done

--- 03_Classification/01_K_Nearest_Neighbor/MyKNN.py
+++ 03_Classification/01_K_Nearest_Neighbor/MyKNN.py
@@ -2,12 +2,30 @@ import numpy as np
 import pandas as pd
 import scipy as sp
 
-class MyKNN():
+class MyKNeighborsClassifier():
+    """
+    My implementation of KNN algorithm.
+    """
     
     def __init__(self, n_neighbors=5):
         self.n_neighbors=n_neighbors
         
     def fit(self, X, y):
+        """
+        Fit the model using X as array of features and y as array of labels.
+        """
+        n_samples = X.shape[0]
+        # number of neighbors can't be larger then number of samples
+        if self.n_neighbors > n_samples:
+            raise ValueError("Number of neighbors can't be larger then number of samples in training set.")
+        
+        # X and y need to have the same number of samples
+        if X.shape[0] != y.shape[0]:
+            raise ValueError("Number of samples in X and y need to be equal.")
+        
+        # finding and saving all possible class labels
+        self.classes_ = np.unique(y)
+        
         self.X = X
         self.y = y
         
@@ -21,6 +39,7 @@ class MyKNN():
         
         # loop over all observations
         for i in range(n_predictions):
+            # calculation of single prediction
             predictions[i] = single_prediction(self.X, self.y, X_test[i, :], self.n_neighbors)
 
         return(predictions)
@@ -33,16 +52,19 @@ def single_prediction(X, y, x_train, k):
     
     # create array for distances and targets
     distances = np.empty(n_samples, dtype=np.float64)
-    targets = np.empty(k, dtype=int)
-    
+
+    # distance calculation
     for i in range(n_samples):
         distances[i] = (x_train - X[i]).dot(x_train - X[i])
-        
+    
+    # combining arrays as columns
     distances = sp.c_[distances, y]
+    # sorting array by value of first column
     sorted_distances = distances[distances[:,0].argsort()]
+    # celecting labels associeted with k smallest distances
     targets = sorted_distances[0:k,1]
+
     unique, counts = np.unique(targets, return_counts=True)
-   
     return(unique[np.argmax(counts)])
     
 
@@ -61,7 +83,7 @@ y = le.fit_transform(y)
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-classifier = MyKNN(n_neighbors=10)
+classifier = MyKNeighborsClassifier(n_neighbors=10)
 classifier.fit(X_train, y_train)
 print(classifier.predict(X_test))
 
