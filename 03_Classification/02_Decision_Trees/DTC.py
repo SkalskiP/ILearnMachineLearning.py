4b0be405d39528fc5633b0ae16eede57047a838d
SkalskiP <pskalski@student.agh.edu.pl>
2017-09-29 00:26:16 +0200

    get_split updated

--- 03_Classification/02_Decision_Trees/DTC.py
+++ 03_Classification/02_Decision_Trees/DTC.py
@@ -1,8 +1,18 @@
 import numpy as np
 
-test = np.array([0, 0, 1, 1, 0, 0, 1, 1])
-test1 = [np.array([0, 0, 1, 1]), np.array([0, 0, 1, 1])]
+labels = np.array([0, 0, 0, 0, 0, 1, 1, 1, 1, 1])
 classes = np.array([0, 1])
+features = np.array(
+    [[  2.77124472,   1.78478393],
+     [  1.72857131,   1.16976141],
+     [  3.67831985,   2.81281357],
+     [  3.96104336,   2.61995032],
+     [  2.99920892,   2.20901421],
+     [  7.49754587,   3.16295355],
+     [  9.00220326,   3.33904719],
+     [  7.44454233,   0.47668338],
+     [ 10.12493903,   3.23455098],
+     [  6.64228735,   3.31998376]])
 
 def gini_index(groups_label, classes):
     """
@@ -40,29 +50,42 @@ def gini_index(groups_label, classes):
         gini_index += (1 - score) * (g_samples/n_samples)
     return gini_index
 
-def get_split(labels, classes):
+def get_split(features, labels, classes):
     """
     Select the best split point for a dataset
     """
     # Number of samles in dataset
     n_samples = labels.shape[0]
     
+    # Number of features in dataset
+    n_features = features.shape[1]
+    
     # Best index and best value of gini index
-    b_index, b_gini = None, None
+    b_dim, b_index, b_gini, array_1, array_2 = None, None, None, None, None
 
-    # Iteration over possible split points
-    for i in range(1, n_samples):
-        # Pair of arrays after splitting
-        spited = np.split(labels, [i])
-        # Calculated gini value
-        gini_val = gini_index(spited, classes)
-        # Checking if we found better split
-        if b_gini == None or gini_val < b_gini:
-            b_gini = gini_val
-            b_index = i
+    # Iteration over dimention
+    for d in range(0, n_features):
+        #Sorting features and labels by values of d-th feature
+        order = features[:, d].argsort()
+        features_sorted = features[order]
+        labels_sorted = labels[order]
+
+        # Iteration over possible split points
+        for i in range(1, n_samples):
+            # Pair of arrays after splitting
+            spited = np.split(labels_sorted, [i])
+            # Calculated gini value
+            gini_val = gini_index(spited, classes)
+            # Checking if we found better split
+            if b_gini == None or gini_val < b_gini:
+                b_gini = gini_val
+                b_index = i
+                b_dim = d
+                array_1 = spited
+                array_2 = np.split(features_sorted, [i])
             
-    return b_index
+    return b_dim, b_index, array_1, array_2
         
-print(get_split(test, classes))
+print(get_split(features, labels, classes))
 
         
