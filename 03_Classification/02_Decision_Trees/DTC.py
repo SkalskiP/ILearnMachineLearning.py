10710f3258c2bd5a38011527b6cb6de79296a48b
SkalskiP <pskalski@student.agh.edu.pl>
2017-09-30 11:54:30 +0200

    DTC class is working

--- 03_Classification/02_Decision_Trees/DTC.py
+++ 03_Classification/02_Decision_Trees/DTC.py
@@ -6,28 +6,109 @@ class Tree():
         # If this node is leaf this field wil hold label of class that is predicted
         self.label = label
         # Index of feature that we need to check
-        self.dimention = dimention
+        self.dim = dimention
         # Value of featue that we need to chceck
         self.value = value
         # Left leaf
         self.left = None
         # Right leaf
         self.right = None
+    
+class MyDecisionTreeClassifier():
+    """
+    criterion : The function to measure the quality of a split.
+    
+    max_depth : The maximum depth of the tree. If None, then nodes are expanded until
+                all leaves are pure or until all leaves contain less than
+                min_samples_split samples.
+    
+    min_samples_split : The minimum number of samples required to split an internal node.
+    
+    min_samples_leaf : The minimum number of samples required to be at a leaf node.
+    """
+    def __init__(self, 
+                 criterion="gini", 
+                 max_depth = 100, 
+                 min_samples_split=2):
+        
+        self.criterion = criterion
+        self.max_depth = max_depth
+        self.min_samples_split = min_samples_split
+        
+    def fit(self, X, y):
+        """
+        Method to build a decision tree classifier from the training set (X, y).
+        
+        X : Array of features values; shape = [n_samples, n_features]
+        
+        y : Array of labels; shape = [n_samples]
+        """
+        
+        # X and y need to have the same number of samples
+        if X.shape[0] != y.shape[0]:
+            raise ValueError("Number of samples in X and y need to be equal.")
+        
+        # Finding and saving all possible class labels
+        self.classes_ = np.unique(y)
+        
+        self.X = X
+        self.y = y
 
-labels = np.array([0, 0, 0, 0, 0, 1, 1, 1, 1, 1])
-classes = np.array([0, 1])
-features = np.array(
-    [[  2.77124472,   1.78478393],
-     [  1.72857131,   1.16976141],
-     [  3.67831985,   2.81281357],
-     [  3.96104336,   2.61995032],
-     [  2.99920892,   2.20901421],
-     [  7.49754587,   3.16295355],
-     [  9.00220326,   3.33904719],
-     [  7.44454233,   0.47668338],
-     [ 10.12493903,   3.23455098],
-     [  6.64228735,   3.31998376]])
+        # Root of decision tree
+        self.tree_ = Tree()
+        # Building a tree
+        self.split(self.X, self.y, self.tree_, 1)
+        
+        
+    def split(self, features, labels, node, depth):
+        """
+        Create child splits for a node or make terminal.
+        """
+        
+        # if set of samples is too small
+        # or node depth is to big
+        #we take as label the most frequent class
+        if(labels.shape[0] <= self.min_samples_split or depth >= self.max_depth):
+            unique, counts = np.unique(labels, return_counts=True)
+            node.label = unique[np.argmax(counts)]
+            return
+        
+        gini_value, split_dim, split_value, spl_labels, spl_features = get_split(features, labels, self.classes_)
+        # Building node children
+        
+        node.left = Tree()
+        node.right = Tree()
+        """    
+        # Stop growing if split is pure
+        if(gini_value == 0):
+            node.left.label = spl_labels[0][0]
+            node.right.label = spl_labels[1][0]
+            return
+        """  
+        node.value = split_value
+        node.dim = split_dim
+        
+        print("Dim: " + str(node.dim) + " and Value: " + str(node.value))
+        
+        self.split(spl_features[0], spl_labels[0], node.left, depth+1)
+        self.split(spl_features[1], spl_labels[1], node.right, depth+1)
+        
+    def predict(self, X_test):
+        predictions = np.empty(X_test.shape[0])
+        for index, sample in enumerate(X_test):
+            predictions[index] = single_prdict(sample, self.tree_)
+    
+        return predictions
+        
+def single_prdict(features, node):
+    while node.label == None:
+        if features[node.dim] > node.value:
+            node = node.right
+        else:
+            node = node.left
+    return node.label
 
+    
 def gini_index(groups_label, classes):
     """
     Method to calculate Gini Index for split dataset.
@@ -95,11 +176,75 @@ def get_split(features, labels, classes):
                 b_gini = gini_val
                 b_index = i
                 b_dim = d
-                b_value = features_sorted[i, d]
+                b_value = (features_sorted[i, d] + features_sorted[i-1, d])/2
                 array_1 = spited
                 array_2 = np.split(features_sorted, [i])
             
-    return b_gini, b_dim, b_index, b_value, array_1, array_2
-        
-print(get_split(features, labels, classes))
+    return b_gini, b_dim, b_value, array_1, array_2
 
+import pandas as pd
+dataset = pd.read_csv('../../00_Datasets/Iris.csv')
+
+feature_columns = ['SepalLengthCm', 'PetalWidthCm']
+X = dataset[feature_columns].values
+y = dataset['Species'].values
+
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+
+labels = np.array([0, 1, 1, 1, 0, 1, 1, 0, 0, 0])
+classes = np.array([0, 1])
+features = np.array(
+    [[  2.77124472,   1.78478393],
+     [  1.72857131,   1.16976141],
+     [  3.67831985,   2.81281357],
+     [  3.96104336,   2.61995032],
+     [  2.99920892,   2.20901421],
+     [  7.49754587,   3.16295355],
+     [  9.00220326,   3.33904719],
+     [  7.44454233,   0.47668338],
+     [ 10.12493903,   3.23455098],
+     [  6.64228735,   3.31998376]])
+   
+# print(get_split(features, labels, classes))
+
+Test = MyDecisionTreeClassifier()
+Test.fit(X_train, y_train)
+# print(Test.tree_.left.value)
+# print(Test.tree_.right.value)
+y_pred_train = Test.predict(X_train)
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_train, y_pred_train)
+print(cm)
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+plt.figure()
+plt.figure(figsize=(12,8))
+plt.title('The optimal number of neighbors', fontsize=20, fontweight='bold')
+plt.xlabel('Number of Neighbors K', fontsize=15)
+plt.ylabel('Misclassification Error', fontsize=15)
+sns.set_style("whitegrid")
+
+# tworzymy siatke pixeli, która posłuży do wizualizacji klastrów
+X1, X2 = np.meshgrid(np.arange(start = X_train[:,0].min() - 1, stop = X_train[:, 0].max() + 1, step = 0.02),
+                     np.arange(start = X_train[:,1].min() - 1, stop = X_train[:, 1].max() + 1, step = 0.02))
+
+# wizualizacja pól klastrów
+plt.contourf(X1, X2, Test.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+            alpha = 0.2, cmap = plt.cm.summer)
+
+# granice wykresów
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+
+plt.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap = plt.cm.summer)
+
+plt.show()
