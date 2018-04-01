26a5e0e76e81134cb9ef8c07c0ca2e5906c6f567
SkalskiP <pskalski@student.agh.edu.pl>
2018-04-01 16:59:07 +0200

    my new features

--- 99_Kaggle_competitions/TalkingDataCompetition/scripts/20180330_my_lightgbm_5/script.py
+++ 99_Kaggle_competitions/TalkingDataCompetition/scripts/20180330_my_lightgbm_5/script.py
@@ -1,39 +1,22 @@
-# Version 5 of this kernel is a Python version of Pranav Pandya's R LightGBM:
-#    https://www.kaggle.com/pranav84/single-lightgbm-in-r-with-75-mln-rows-lb-0-9690
+import pandas as pd
+import time
+import gc
+import numpy as np
+from sklearn.model_selection import train_test_split 
+import lightgbm as lgb
+from datetime import datetime
 
-# Version 6 increases MAX_ROUNDS to get early stopping
-# Version 7 runs without validation based on the result of version 6
-# Version 9 (still trying to run full training)
-#           gets rid of AUC evals, to maybe speed it up
-# Version 10 runs validation with shuffle=False
-#            (since there is a relevant time element
-#             which will leak in shuffled validation)
-# Version 11 runs without validation based on result of version 11
-# Version 12 adds 'day' restriction to all aggregations
-#            (to make train and test sets more comparable)
-# Version 13 attempts to expand the data back by 25 million records
-# Version 14 runs without validation based on result of version 13
-# Version 15 even more records
-# Version 16 OK, not quite so many records
-# Version 17 reverts to version 14 (LB=.9694)
-
-VALIDATE = True
-
+# Constants and general settings
 MAX_ROUNDS = 1000
 EARLY_STOP = 50
 OPT_ROUNDS = 680
 
+# Paths and file names settings
 FULL_OUTFILE = 'sub_lgbm_r_to_python_nocv.csv'
 VALID_OUTFILE = 'sub_lgbm_r_to_python_withcv.csv'
-
-import pandas as pd
-import time
-import numpy as np
-from sklearn.model_selection import train_test_split 
-import lightgbm as lgb
-
 path = '../../input/'
 
+# Columns types
 dtypes = {
     'ip'            : 'uint32',
     'app'           : 'uint16',
@@ -44,102 +27,165 @@ dtypes = {
     'click_id'      : 'uint32'
 }
 
-print('load train...')
+# Columns names
 train_cols = ['ip','app','device','os', 'channel', 'click_time', 'is_attributed']
-train_df = pd.read_csv(path+"train.csv", skiprows=range(1,84903891), nrows=100000000,dtype=dtypes, usecols=train_cols)
+test_cols = ['ip','app','device','os', 'channel', 'click_time', 'click_id']
 
-import gc
+target = 'is_attributed'
+predictors = ['app',
+              'device',
+              'os',
+              'channel',
+              'hour',
+              'day',
+              'minute',
+              'second',
+              'ip_hr',
+              'ip_hr_app',
+              'ip_hr_os',
+              'ip_hr_dev',
+              'ip_min',
+              'ip_min_app',
+              'ip_min_os',
+              'ip_min_dev']
 
-len_train = len(train_df)
+categorical = ['app', 'device', 'os', 'channel', 'hour']
 
-gc.collect()
 
-print('data prep...')
+# Limit dates for the training and test set
+train_start_date = "2017-11-08 00:00:00"
+train_end_date = "2017-11-08 23:59:59"
+valid_start_date = "2017-11-09 04:00:00"
+valid_end_date = "2017-11-09 16:00:00"
 
-most_freq_hours_in_test_data = [4, 5, 9, 10, 13, 14]
-least_freq_hours_in_test_data = [6, 11, 15]
+train_start_date = datetime.strptime(train_start_date, '%Y-%m-%d %H:%M:%S')
+train_end_date = datetime.strptime(train_end_date, '%Y-%m-%d %H:%M:%S')
+valid_start_date = datetime.strptime(valid_start_date, '%Y-%m-%d %H:%M:%S')
+valid_end_date = datetime.strptime(valid_end_date '%Y-%m-%d %H:%M:%S')
 
+# Filtration by date
+def filtrationByDateTrain(df):
+    print("Converting to datetime...")
+    df['click_time'] = pd.to_datetime(df['click_time'])
+    print("Filtration of dataset...")
+    return df[(df['click_time'] <= train_end_date) & (df['click_time'] > train_start_date)]
+
+def filtrationByDateValid(df):
+    print("Converting to datetime...")
+    df['click_time'] = pd.to_datetime(df['click_time'])
+    print("Filtration of dataset...")
+    return df[(df['click_time'] <= valid_end_date) & (df['click_time'] > test_start_date)]
 
 def prep_data( df ):
-    
-    df['hour'] = pd.to_datetime(df.click_time).dt.hour.astype('uint8')
-    df['day'] = pd.to_datetime(df.click_time).dt.day.astype('uint8')
+    print('Prep data...')
+
+    print("Extract day and hour from datetime...")
+    df['hour'] = df['click_time'].dt.hour.astype('uint8')
+    df['day'] = df['click_time'].dt.dayofyear.astype('uint8')
+    df['minute'] = df['click_time'].dt.minute.astype('uint8')
+    df['second'] = df['click_time'].dt.second.astype('uint8')
     df.drop(['click_time'], axis=1, inplace=True)
     gc.collect()
     
-    df['in_test_hh'] = (   3 
-                         - 2*df['hour'].isin(  most_freq_hours_in_test_data ) 
-                         - 1*df['hour'].isin( least_freq_hours_in_test_data ) ).astype('uint8')
-    print( df.info() )
-
-    print('group by : ip_day_test_hh')
-    gp = df[['ip', 'day', 'in_test_hh', 'channel']].groupby(by=['ip', 'day',
-             'in_test_hh'])[['channel']].count().reset_index().rename(index=str, 
-             columns={'channel': 'nip_day_test_hh'})
-    df = df.merge(gp, on=['ip','day','in_test_hh'], how='left')
-    del gp
-    df.drop(['in_test_hh'], axis=1, inplace=True)
-    print( "nip_day_test_hh max value = ", df.nip_day_test_hh.max() )
-    df['nip_day_test_hh'] = df['nip_day_test_hh'].astype('uint32')
-    gc.collect()
-    print( df.info() )
-
-    print('group by : ip_day_hh')
-    gp = df[['ip', 'day', 'hour', 'channel']].groupby(by=['ip', 'day', 
+    print("Aggregation of features inside hour...")
+    
+    print("Group by ip, day, hour...")
+    gp = df[['ip', 'day', 'hour', 'channel']].groupby(by=['ip', 'day',
              'hour'])[['channel']].count().reset_index().rename(index=str, 
-             columns={'channel': 'nip_day_hh'})
+             columns={'channel': 'ip_hr'})
     df = df.merge(gp, on=['ip','day','hour'], how='left')
+    df['ip_hr'] = df['ip_hr'].astype('uint32')
+    print( "ip_hr max val = ", df.ip_hr.max() )
     del gp
-    print( "nip_day_hh max value = ", df.nip_day_hh.max() )
-    df['nip_day_hh'] = df['nip_day_hh'].astype('uint16')
     gc.collect()
+    
+    print("Group by ip, day, hour, app...")
+    gp = df[['ip', 'day', 'hour', 'app', 'channel']].groupby(by=['ip', 'day',
+             'hour', 'app'])[['channel']].count().reset_index().rename(index=str, 
+             columns={'channel': 'ip_hr_app'})
+    df = df.merge(gp, on=['ip','day','hour','app'], how='left')
+    df['ip_hr_app'] = df['ip_hr_app'].astype('uint32')
+    print( "ip_hr_app max val = ", df.ip_hr_app.max() )
+    del gp
+    gc.collect()
+    
+    print("Group by ip, day, hour, os...")
+    gp = df[['ip', 'day', 'hour', 'os', 'channel']].groupby(by=['ip', 'day',
+             'hour', 'os'])[['channel']].count().reset_index().rename(index=str, 
+             columns={'channel': 'ip_hr_os'})
+    df = df.merge(gp, on=['ip','day','hour','os'], how='left')
+    df['ip_hr_os'] = df['ip_hr_os'].astype('uint32')
+    print( "ip_hr_os max val = ", df.ip_hr_os.max() )
+    del gp
+    gc.collect()
+    
+    print("Group by ip, day, hour, device...")
+    gp = df[['ip', 'day', 'hour', 'device', 'channel']].groupby(by=['ip', 'day',
+             'hour', 'device'])[['channel']].count().reset_index().rename(index=str, 
+             columns={'channel': 'ip_hr_dev'})
+    df = df.merge(gp, on=['ip','day','hour','device'], how='left')
+    df['ip_hr_dev'] = df['ip_hr_dev'].astype('uint32')
+    print( "ip_hr_dev max val = ", df.ip_hr_dev.max() )
+    del gp
+    gc.collect()
+    
+    print("Aggregation of features inside minute...")
+    
+    print("Group by ip, day, hour, minute...")
+    gp = df[['ip', 'day', 'hour', 'minute', 'channel']].groupby(by=['ip', 'day',
+             'hour','minute'])[['channel']].count().reset_index().rename(index=str, 
+             columns={'channel': 'ip_min'})
+    df = df.merge(gp, on=['ip','day','hour', 'minute'], how='left')
+    df['ip_min'] = df['ip_min'].astype('uint32')
+    print( "ip_min max val = ", df.ip_min.max() )
+    del gp
+    gc.collect()
+    
+    print("Group by ip, day, hour, minute, app...")
+    gp = df[['ip', 'day', 'hour', 'minute', 'app', 'channel']].groupby(by=['ip', 'day',
+             'hour', 'minute', 'app'])[['channel']].count().reset_index().rename(index=str, 
+             columns={'channel': 'ip_min_app'})
+    df = df.merge(gp, on=['ip','day','hour','minute','app'], how='left')
+    df['ip_min_app'] = df['ip_min_app'].astype('uint32')
+    print( "ip_min_app max val = ", df.ip_min_app.max() )
+    del gp
+    gc.collect()
+    
+    print("Group by ip, day, hour, minute, os...")
+    gp = df[['ip', 'day', 'hour', 'minute', 'os', 'channel']].groupby(by=['ip', 'day',
+             'hour', 'minute', 'os'])[['channel']].count().reset_index().rename(index=str, 
+             columns={'channel': 'ip_min_os'})
+    df = df.merge(gp, on=['ip','day','hour','minute','os'], how='left')
+    df['ip_min_os'] = df['ip_min_os'].astype('uint32')
+    print( "ip_min_os max val = ", df.ip_min_os.max() )
+    del gp
+    gc.collect()
+    
+    print("Group by ip, day, hour, minute, device...")
+    gp = df[['ip', 'day', 'hour', 'minute', 'device', 'channel']].groupby(by=['ip', 'day',
+             'hour', 'minute', 'device'])[['channel']].count().reset_index().rename(index=str, 
+             columns={'channel': 'ip_min_dev'})
+    df = df.merge(gp, on=['ip','day','hour','minute','device'], how='left')
+    df['ip_min_dev'] = df['ip_min_dev'].astype('uint32')
+    print( "ip_min_dev max val = ", df.ip_min_dev.max() )
+    del gp
+    gc.collect()
+    
+    print("Aggregation of features inside second...")
+    
+    print("Group by ip, day, hour, minute, second...")
+    gp = df[['ip', 'day', 'hour', 'minute', 'second', 'channel']].groupby(by=['ip', 'day',
+             'hour','minute', 'second'])[['channel']].count().reset_index().rename(index=str, 
+             columns={'channel': 'ip_sec'})
+    df = df.merge(gp, on=['ip','day','hour', 'minute', 'second'], how='left')
+    df['ip_sec'] = df['ip_sec'].astype('uint32')
+    print( "ip_sec max val = ", df.ip_sec.max() )
+    del gp
+    gc.collect()
+    
     print( df.info() )
 
-    print('group by : ip_hh_os')
-    gp = df[['ip', 'day', 'os', 'hour', 'channel']].groupby(by=['ip', 'os', 'day',
-             'hour'])[['channel']].count().reset_index().rename(index=str, 
-             columns={'channel': 'nip_hh_os'})
-    df = df.merge(gp, on=['ip','os','hour','day'], how='left')
-    del gp
-    print( "nip_hh_os max value = ", df.nip_hh_os.max() )
-    df['nip_hh_os'] = df['nip_hh_os'].astype('uint16')
-    gc.collect()
-    print( df.info() )
-
-    print('group by : ip_hh_app')
-    gp = df[['ip', 'app', 'hour', 'day', 'channel']].groupby(by=['ip', 'app', 'day',
-             'hour'])[['channel']].count().reset_index().rename(index=str, 
-             columns={'channel': 'nip_hh_app'})
-    df = df.merge(gp, on=['ip','app','hour','day'], how='left')
-    del gp
-    print( "nip_hh_app max value = ", df.nip_hh_app.max() )
-    df['nip_hh_app'] = df['nip_hh_app'].astype('uint16')
-    gc.collect()
-    print( df.info() )
-
-    print('group by : ip_hh_dev')
-    gp = df[['ip', 'device', 'hour', 'day', 'channel']].groupby(by=['ip', 'device', 'day',
-             'hour'])[['channel']].count().reset_index().rename(index=str, 
-             columns={'channel': 'nip_hh_dev'})
-    df = df.merge(gp, on=['ip','device','day','hour'], how='left')
-    del gp
-    print( "nip_hh_dev max value = ", df.nip_hh_dev.max() )
-    df['nip_hh_dev'] = df['nip_hh_dev'].astype('uint32')
-    gc.collect()
-    print( df.info() )
-
-    print('group by : app_hh')
-    gp = df[['app', 'hour', 'day', 'channel']].groupby(by=['app', 'day',
-             'hour'])[['channel']].count().reset_index().rename(index=str, 
-             columns={'channel': 'app_hh'})
-    df = df.merge(gp, on=['app','day','hour'], how='left')
-    del gp
-    print( "app_hh max value = ", df.nip_hh_dev.max() )
-    df['app_hh'] = df['app_hh'].astype('uint32')
-    gc.collect()
-    print( df.info() )
-
-    df.drop( ['ip','day', 'device'], axis=1, inplace=True )
+    df.drop( ['ip','day'], axis=1, inplace=True )
     gc.collect()
     print( df.info() )
     
@@ -147,10 +193,19 @@ def prep_data( df ):
 
 #---------------------------------------------------------------------------------
 
+print('Load test set...')
+test_df = pd.read_csv(path+"test.csv", dtype=dtypes, usecols=test_cols)
+print('Load train set...')
+train_df_pre = pd.read_csv(path+"train.csv", dtype=dtypes, usecols=train_cols)
+
+train_df = filtrationByDateTrain(train_df_pre)
+gc.collect()
+
 print( "Train info before: ")
 print( train_df.info() )
 train_df = prep_data( train_df )
 gc.collect()
+
 print( "Train info after: ")
 print( train_df.info() )
 
@@ -159,119 +214,86 @@ train_df.info()
 
 metrics = 'auc'
 lgb_params = {
-        'boosting_type': 'gbdt',
-        'objective': 'binary',
-        'metric':metrics,
-        'learning_rate': 0.1,
-        'num_leaves': 7,  # we should let it be smaller than 2^(max_depth)
-        'max_depth': 4,  # -1 means no limit
-        'min_child_samples': 100,  # Minimum number of data need in a child(min_data_in_leaf)
-        'max_bin': 100,  # Number of bucketed bin for feature values
-        'subsample': 0.7,  # Subsample ratio of the training instance.
-        'subsample_freq': 1,  # frequence of subsample, <=0 means no enable
-        'colsample_bytree': 0.7,  # Subsample ratio of columns when constructing each tree.
-        'min_child_weight': 0,  # Minimum sum of instance weight(hessian) needed in a child(leaf)
-        'min_split_gain': 0,  # lambda_l1, lambda_l2 and min_gain_to_split to regularization
-        'nthread': 8,
-        'verbose': 0,
-        'scale_pos_weight':99.7, # because training data is extremely unbalanced 
-        'metric':metrics
+    'boosting_type': 'gbdt',
+    'objective': 'binary',
+    'metric':metrics,
+    'learning_rate': 0.1,
+    'num_leaves': 7,  # we should let it be smaller than 2^(max_depth)
+    'max_depth': 4,  # -1 means no limit
+    'min_child_samples': 100,  # Minimum number of data need in a child(min_data_in_leaf)
+    'max_bin': 100,  # Number of bucketed bin for feature values
+    'subsample': 0.7,  # Subsample ratio of the training instance.
+    'subsample_freq': 1,  # frequence of subsample, <=0 means no enable
+    'colsample_bytree': 0.7,  # Subsample ratio of columns when constructing each tree.
+    'min_child_weight': 0,  # Minimum sum of instance weight(hessian) needed in a child(leaf)
+    'min_split_gain': 0,  # lambda_l1, lambda_l2 and min_gain_to_split to regularization
+    'nthread': 8,
+    'verbose': 0,
+    'scale_pos_weight':99.7, # because training data is extremely unbalanced 
+    'metric':metrics
 }
-
-target = 'is_attributed'
-predictors = ['app','os', 'channel', 'hour', 'nip_day_test_hh', 'nip_day_hh',
-              'nip_hh_os', 'nip_hh_app', 'nip_hh_dev', 'app_hh']
-categorical = ['app', 'os', 'channel', 'hour']
 
 print(train_df.head(5))
 
-if VALIDATE:
+print('Load valid set...')
+val_df = filtrationByDateValid(train_df_pre)
+val_df = prep_data( val_df )
+del train_df_pre
+gc.collect()
 
-    train_df, val_df = train_test_split( train_df, train_size=.95, shuffle=False )
+print(train_df.info())
+print(val_df.info())
 
-    print(train_df.info())
-    print(val_df.info())
+print("train size: ", len(train_df))
+print("valid size: ", len(val_df))
 
-    print("train size: ", len(train_df))
-    print("valid size: ", len(val_df))
+gc.collect()
 
-    gc.collect()
+print("Training...")
 
-    print("Training...")
+num_boost_round=MAX_ROUNDS
+early_stopping_rounds=EARLY_STOP
 
-    num_boost_round=MAX_ROUNDS
-    early_stopping_rounds=EARLY_STOP
+xgtrain = lgb.Dataset(train_df[predictors].values, label=train_df[target].values,
+                        feature_name=predictors,
+                        categorical_feature=categorical
+                        )
+del train_df
+gc.collect()
 
-    xgtrain = lgb.Dataset(train_df[predictors].values, label=train_df[target].values,
-                          feature_name=predictors,
-                          categorical_feature=categorical
-                          )
-    del train_df
-    gc.collect()
+xgvalid = lgb.Dataset(val_df[predictors].values, label=val_df[target].values,
+                        feature_name=predictors,
+                        categorical_feature=categorical
+                        )
+del val_df
+gc.collect()
 
-    xgvalid = lgb.Dataset(val_df[predictors].values, label=val_df[target].values,
-                          feature_name=predictors,
-                          categorical_feature=categorical
-                          )
-    del val_df
-    gc.collect()
+evals_results = {}
 
-    evals_results = {}
+bst = lgb.train(lgb_params, 
+                    xgtrain, 
+                    valid_sets= [xgvalid], 
+                    valid_names=['valid'], 
+                    evals_result=evals_results, 
+                    num_boost_round=num_boost_round,
+                    early_stopping_rounds=early_stopping_rounds,
+                    verbose_eval=10, 
+                    feval=None)
 
-    bst = lgb.train(lgb_params, 
-                     xgtrain, 
-                     valid_sets= [xgvalid], 
-                     valid_names=['valid'], 
-                     evals_result=evals_results, 
-                     num_boost_round=num_boost_round,
-                     early_stopping_rounds=early_stopping_rounds,
-                     verbose_eval=10, 
-                     feval=None)
+n_estimators = bst.best_iteration
 
-    n_estimators = bst.best_iteration
+print("\nModel Report")
+print("n_estimators : ", n_estimators)
+print(metrics+":", evals_results['valid'][metrics][n_estimators-1])
 
-    print("\nModel Report")
-    print("n_estimators : ", n_estimators)
-    print(metrics+":", evals_results['valid'][metrics][n_estimators-1])
-    
-    outfile = VALID_OUTFILE
-    
-    del xgvalid
+outfile = VALID_OUTFILE
 
-else:
-
-    print(train_df.info())
-
-    print("train size: ", len(train_df))
-
-    gc.collect()
-
-    print("Training...")
-
-    num_boost_round=OPT_ROUNDS
-
-    xgtrain = lgb.Dataset(train_df[predictors].values, label=train_df[target].values,
-                          feature_name=predictors,
-                          categorical_feature=categorical
-                          )
-    del train_df
-    gc.collect()
-
-    bst = lgb.train(lgb_params, 
-                     xgtrain, 
-                     num_boost_round=num_boost_round,
-                     verbose_eval=10, 
-                     feval=None)
-                     
-    outfile = FULL_OUTFILE
-
+del xgvalid
 del xgtrain
 gc.collect()
 
-print('load test...')
-test_cols = ['ip','app','device','os', 'channel', 'click_time', 'click_id']
-test_df = pd.read_csv(path+"test.csv", dtype=dtypes, usecols=test_cols)
-
+print('Load test set...')
+test_df = filtrationByDateTest(test_df)
 test_df = prep_data( test_df )
 gc.collect()
 
