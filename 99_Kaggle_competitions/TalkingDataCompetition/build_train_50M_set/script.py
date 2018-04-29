5a9bf88184f892fb79f6de941f3bfc8ec208406b
SkalskiP <pskalski@student.agh.edu.pl>
2018-04-29 14:17:28 +0000

    subsets builder

---
+++ 99_Kaggle_competitions/TalkingDataCompetition/build_train_50M_set/script.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+# File settings
+INPUT_FILE = "../input/train.csv"
+OUTPUT_FILE = "../input/train50M.h5"
+
+TOTAL_ROWS = 184903891-1
+CHUNK_SIZE = 50000000
+START_POINT = TOTAL_ROWS - CHUNK_SIZE
+
+# Columns types
+dtype = {
+    'ip'            : 'uint32',
+    'app'           : 'uint16',
+    'device'        : 'uint16',
+    'os'            : 'uint16',
+    'channel'       : 'uint16',
+    'is_attributed' : 'uint8',
+    'click_id'      : 'uint32',
+}
+
+# Column names
+usecols = [
+    'ip',
+    'app',
+    'device',
+    'os',
+    'channel',
+    'click_time',
+    'is_attributed'
+]
+
+print('loading full data set...')
+train_df = pd.read_csv(INPUT_FILE, parse_dates=['click_time'], dtype=dtype, skiprows=range(1, START_POINT), nrows=CHUNK_SIZE, usecols=usecols)
+print('saving to file...')
+train_df.to_hdf(OUTPUT_FILE, 'train_df_50', format ="fixed", mode="w")
---
+++ 99_Kaggle_competitions/TalkingDataCompetition/build_validation_set/script.py
@@ -0,0 +1,38 @@
+import pandas as pd
+
+# File settings
+INPUT_FILE = "../input/train.csv"
+OUTPUT_FILE = "../input/valid.h5"
+
+# Date settings
+START_DATE = "2017-11-09 4:00:00"
+END_DATE = "2017-11-09 16:00:00"
+
+# Columns types
+dtype = {
+    'ip'            : 'uint32',
+    'app'           : 'uint16',
+    'device'        : 'uint16',
+    'os'            : 'uint16',
+    'channel'       : 'uint16',
+    'is_attributed' : 'uint8',
+    'click_id'      : 'uint32',
+}
+
+# Column names
+usecols = [
+    'ip',
+    'app',
+    'device',
+    'os',
+    'channel',
+    'click_time',
+    'is_attributed'
+]
+
+print('loading full data set...')
+full_df = pd.read_csv(INPUT_FILE, parse_dates=['click_time'], dtype=dtype, usecols=usecols)
+print('filtering by date...')
+valid_df = full_df.loc[(full_df['click_time'] < END_DATE) & (full_df['click_time'] > START_DATE)]
+print('saving to file...')
+valid_df.to_hdf(OUTPUT_FILE, 'df', format ="fixed", mode="w")
